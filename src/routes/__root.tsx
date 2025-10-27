98bc14d947571ea48b2180e24c1ff4458415bcc6
Dhaniel Sales <dhanielr94@gmail.com>
2025-10-27 19:42:14 -0300

    fixing configs and removing devtools

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,4 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { Toaster } from "react-hot-toast";
 import { Layout } from "../components/Layout";
 
@@ -33,7 +32,6 @@ export const Route = createRootRoute({
           },
         }}
       />
-      <TanStackRouterDevtools />
     </>
   ),
 });
